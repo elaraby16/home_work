ba9a4f8505bd92de2223a142e3d5171869868e62
elaraby <m.y.elaraby6@gmail.com>
2026-02-10 18:45:45 +0200

    .

--- homeWork7/q7.dart
+++ homeWork7/q7.dart
@@ -1,15 +1,19 @@
-// Q6 Create a class NumberCheck with an attribute value. Add a method isEven() that returns true if
-// the number is even, false otherwise. In main(), test the method with one number.
-class NumberCheck {
-  int value;
-
-  NumberCheck(this.value);
-
-  bool isEven() {
-    return value % 2 == 0;
-  }
-}
+// Q7 Ask the user for a number (e.g., 9875). Keep summing its digits until the result is a single digit.
+// Print the final single-digit result. (Example: 9+8+7+5 = 29 → 2+9 = 11 → 1+1 = 2)
+import 'dart:io';
 void main() {
-  NumberCheck number = NumberCheck(10);
-  print('Number: ${number.value}, isEven: ${number.isEven()}');
+  print('Enter a number:');
+  String input = stdin.readLineSync() ?? '';
+  int number = int.tryParse(input) ?? 0;
+
+  while (number >= 10) {
+    int sum = 0;
+    while (number > 0) {
+      sum += number % 10;
+      number ~/= 10;
+    }
+    number = sum;
+  }
+
+  print('The final single-digit result is: $number');
 }
